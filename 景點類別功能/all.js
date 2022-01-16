84383f631b1337deb05bb9dc3fb19b7042021ae0
oli2022 <oli0970717429@gmail.com>
2022-01-16 08:38:38 +0800

    將資料渲染到畫面上

--- 景點類別功能/all.js
+++ 景點類別功能/all.js
@@ -1,3 +1,31 @@
+const list = document.querySelector(".list");
+const tourList = document.querySelector(".tourList");
+list.addEventListener("click", function (e) {
+    //中止網頁連結
+    e.preventDefault();
+    if (e.target.nodeName !== "A") {
+        return;
+    }
+    const categoly = e.target.getAttribute("data-categoly");
+    axios
+        .get(
+            `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?%24filter=contains(Class1,'${categoly}')&%24format=JSON`,
+            {
+                headers: getAuthorizationHeader(),
+            }
+        )
+        .then(function (response) {
+            const thisData = response.data;
+            console.log(thisData);
+            let str = "";
+            thisData.forEach(function (item){
+                str += `<li><h2>${item.ScenicSpotName}</h2>
+                ${item.DescriptionDetail}
+                </li>`
+            })
+            tourList.innerHTML = str;
+        });
+});
 function getAuthorizationHeader() {
     //  填入自己 ID、KEY 開始
     let AppID = "28be96d18851420bb6b4b4aa24cad688";
