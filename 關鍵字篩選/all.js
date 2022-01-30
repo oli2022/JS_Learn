e6290bc7ffbad90a0adfd4b0efd0fda0c72276c9
oli2022 <oli0970717429@gmail.com>
2022-01-30 11:15:25 +0800

    關鍵字輸入後可搜尋全台景點

--- 關鍵字篩選/all.js
+++ 關鍵字篩選/all.js
@@ -1,3 +1,46 @@
+const txt = document.querySelector(".txt");
+const send = document.querySelector(".send");
+const list = document.querySelector(".list");
+send.addEventListener("click", function (e) {
+    const keyWord = txt.value;
+    txt.value = "";
+    axios
+        .get(
+            `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24filter=contains(ScenicSpotName,'${keyWord}')&%24format=JSON`,
+            {
+                headers: getAuthorizationHeader(),
+            }
+        )
+        .then(function (response) {
+            const thisData = response.data;
+            let str = "";
+            thisData.forEach(function (item) {
+                str += `<li><a href="page.html?id=${item.ScenicSpotID}>${item.ScenicSpotName}</a></li>`;
+            });
+            list.innerHTML = str;
+        });
+});
+function init() {
+    getAllTour();
+}
+init();
+function getAllTour() {
+    axios
+        .get(
+            "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24filter=contains(ScenicSpotName%2C'%E8%8A%B1')&%24top=30&%24format=JSON",
+            {
+                headers: getAuthorizationHeader(),
+            }
+        )
+        .then(function (response) {
+            const thisData = response.data;
+            let str = "";
+            thisData.forEach(function (item) {
+                str += `<li><a href="page.html?id=${item.ScenicSpotID}>${item.ScenicSpotName}</a></li>`;
+            });
+            list.innerHTML = str;
+        });
+}
 function getAuthorizationHeader() {
     //  填入自己 ID、KEY 開始
     let AppID = "28be96d18851420bb6b4b4aa24cad688";
