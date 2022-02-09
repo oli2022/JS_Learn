8329ece4c5e49da0acb137425c49fd782c0b20b9
oli2022 <oli0970717429@gmail.com>
2022-02-09 23:04:51 +0800

    篩選出當期活動+newDate()

--- 時間區間活動搜尋/all.js
+++ 時間區間活動搜尋/all.js
@@ -1,3 +1,35 @@
+const citySelect = document.querySelector(".citySelect");
+const send = document.querySelector(".send");
+const list = document.querySelector(".list");
+send.addEventListener("click", function (e) {
+    const city = citySelect.value;
+    axios
+        .get(
+            `https://ptx.transportdata.tw/MOTC/v2/Tourism/Activity/${city}?%24format=JSON`,
+            {
+                headers: getAuthorizationHeader(),
+            }
+        )
+        .then(function (response) {
+            const thisData = response.data;
+            console.log(thisData);
+            let str = "";
+            thisData.forEach(function (item) {
+                //const startTime = item.StartTime.substr(0,10);
+                //const endTime = item.EndTime.substr(0,10);
+                //const updateTime = item.UpdateTime.substr(0,10);
+                const startTime = +new Date(item.StartTime);
+                const endTime = +new Date(item.EndTime);
+                const updateTime = +new Date(item.UpdateTime);
+                if (updateTime > startTime && updateTime < endTime) {
+                    str += `<li><h2>${item.ActivityName}</h2><p>(開始 )${startTime}</p><p>(結束 )${endTime}</p>
+                </li>`;
+                } 
+            });
+            list.innerHTML = str;
+        });
+});
+
 function getAuthorizationHeader() {
     //  填入自己 ID、KEY 開始
     let AppID = "28be96d18851420bb6b4b4aa24cad688";
